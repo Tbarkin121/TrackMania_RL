33c0acc0364ec8ca5a9017fc9a2d91b0883e418d
Tyler Barkin <tybarkin@gmail.com>
2021-10-07 15:28:47 -0400

    The env is starting to come together. Need to work on the init function more so that we get into an A01 race from init.

---
+++ TM_Gym/tm_gym/envs/key_input.py
@@ -0,0 +1,70 @@
+# Key Input
+import ctypes
+from time import sleep
+
+A = 0x1E
+B = 0x30
+C = 0x2E
+D = 0x20
+S = 0x1F
+W = 0x11
+
+RETURN = 0x1C 
+RIGHT = 0xCD
+DOWN = 0xD0
+GRAVE = 41
+
+class KeyInput():
+    def __init__(self):
+        self.SendInput = ctypes.windll.user32.SendInput
+
+    def KeyStroke(self, hexKeyCode):
+        self.PressKey(hexKeyCode)
+        sleep(0.1)
+        self.ReleaseKey(hexKeyCode)
+        sleep(0.1)
+
+    def PressKey(self, hexKeyCode):
+        extra = ctypes.c_ulong(0)
+        ii_ = Input_I()
+        ii_.ki = KeyBdInput( 0, hexKeyCode, 0x0008, 0, ctypes.pointer(extra) )
+        x = Input( ctypes.c_ulong(1), ii_ )
+        ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+
+    def ReleaseKey(self, hexKeyCode):
+        extra = ctypes.c_ulong(0)
+        ii_ = Input_I()
+        ii_.ki = KeyBdInput( 0, hexKeyCode, 0x0008 | 0x0002, 0, ctypes.pointer(extra) )
+        x = Input( ctypes.c_ulong(1), ii_ )
+        ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+
+PUL = ctypes.POINTER(ctypes.c_ulong)
+class KeyBdInput(ctypes.Structure):
+    _fields_ = [("wVk", ctypes.c_ushort),
+                ("wScan", ctypes.c_ushort),
+                ("dwFlags", ctypes.c_ulong),
+                ("time", ctypes.c_ulong),
+                ("dwExtraInfo", PUL)]
+
+class HardwareInput(ctypes.Structure):
+    _fields_ = [("uMsg", ctypes.c_ulong),
+                ("wParamL", ctypes.c_short),
+                ("wParamH", ctypes.c_ushort)]
+
+class MouseInput(ctypes.Structure):
+    _fields_ = [("dx", ctypes.c_long),
+                ("dy", ctypes.c_long),
+                ("mouseData", ctypes.c_ulong),
+                ("dwFlags", ctypes.c_ulong),
+                ("time",ctypes.c_ulong),
+                ("dwExtraInfo", PUL)]
+
+class Input_I(ctypes.Union):
+    _fields_ = [("ki", KeyBdInput),
+                ("mi", MouseInput),
+                ("hi", HardwareInput)]
+
+class Input(ctypes.Structure):
+    _fields_ = [("type", ctypes.c_ulong),
+                ("ii", Input_I)]
+
--- TM_Gym/tm_gym/envs/tm_env.py
+++ TM_Gym/tm_gym/envs/tm_env.py
@@ -2,24 +2,53 @@ import gym
 import numpy as np
 import math
 import matplotlib.pyplot as plt
-import subprocess
+import subprocess, os, signal
 from ReadWriteMemory import ReadWriteMemory
 from time import sleep
+# Kill the process using pywin32
+import psutil
+import key_input
 
 class TrackManiaEnv(gym.Env):
     def __init__(self):
         print('Init Track Mania')
-        rwm = ReadWriteMemory()
-        procces_ids_old =  rwm.enumerate_processes()
-        exe_str = '"C:/Program Files (x86)/TmNationsForever/TmForever.exe"'
-        parent = subprocess.Popen(exe_str, stderr=subprocess.PIPE, close_fds=True)
-        procces_ids_new =  rwm.enumerate_processes()
+        self.rwm = ReadWriteMemory()
+        procces_ids_old =  self.rwm.enumerate_processes()
+        sleep(0.1)
+        # exe_name = 'TmForever.exe'
+        exe_name = 'TMInterface'
+        exe_path = 'C:/Program Files (x86)/TmNationsForever/'
+        wd = os.getcwd()
+        os.chdir(exe_path)
+        self.exe_subprocess = subprocess.Popen(exe_name, 
+                                               stdout=subprocess.PIPE,
+                                               stderr=subprocess.PIPE, 
+                                               shell=True,
+                                               close_fds=True)
+        os.chdir(wd)
+        sleep(0.1)
+        procces_ids_new =  self.rwm.enumerate_processes()
         process_ids_diff = list(set(procces_ids_new) - set(procces_ids_old))
         print(process_ids_diff)
-        print(parent)
-        process = rwm.get_process_by_id(process_ids_diff[0])
-        print(process.__dict__)
-        sleep(5)
+        print(self.exe_subprocess)
+        for id in process_ids_diff:
+            self.process = self.rwm.get_process_by_id(id)
+            print(self.process.__dict__)
+            if(self.process.name == 'TmForever.exe'):
+                self.process_id = id
+
+        self.key_input = key_input.KeyInput()
+        self.load_map()
+
+    def load_map(self):
+        print('Loading A01')
+        self.key_input.KeyStroke(0x41)
+        self.key_input.KeyStroke(0x41)
+        self.key_input.KeyStroke(0x1)
+        self.key_input.KeyStroke(0x2)
+        self.key_input.KeyStroke(0x3)
+
+        
     def step(self, action):
         print('Step')
         ob = 0
@@ -38,6 +67,23 @@ class TrackManiaEnv(gym.Env):
 
     def render(self, mode='human'):
         print('Render')
+        if(psutil.pid_exists(self.exe_subprocess.pid)):
+            print('found pid')
+        else:
+            print('didnt find pid')
+        # for proc in psutil.process_iter():
+        #     print(proc.name())
+
 
     def close(self):
         print('Close')
+
+        if(psutil.pid_exists(self.process_id)):
+            print('found pid')
+            proc = psutil.Process(self.process_id)
+            proc.kill()
+        else:
+            print('didnt find pid')
+        
+        # os.kill(self.exe_subprocess.pid, signal.SIGSTOP)
+        # self.exe_subprocess.kill()
